261ff1c73c5231b3b35f109f7534d78434cc2bfb
Hoda03 <62093443+Hoda03@users.noreply.github.com>
2020-12-06 00:44:52 +0100

    Update and rename aboutusFragment.kt to AboutusFragment.kt

--- app/src/main/java/chaymaeidrissi/ma/newsletterapp/fragment/AboutusFragment.kt
+++ app/src/main/java/chaymaeidrissi/ma/newsletterapp/fragment/AboutusFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import chaymaeidrissi.ma.newsletterapp.R
 
-class aboutusFragment: Fragment() {
+class AboutusFragment: Fragment() {
 
     companion object {
         fun newInstance(aboutus: String): aboutusFragment{
