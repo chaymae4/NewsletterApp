88b3ff5b2f0bb81a0797b49edc82cd1f62a2c4ff
Hoda03 <62093443+Hoda03@users.noreply.github.com>
2020-12-06 18:53:19 +0100

    houda

--- app/src/main/java/chaymaeidrissi/ma/newsletterapp/fragment/Favorits.kt
+++ app/src/main/java/chaymaeidrissi/ma/newsletterapp/fragment/Favorits.kt
@@ -1,40 +1,44 @@
 package chaymaeidrissi.ma.newsletterapp.fragment
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import chaymaeidrissi.ma.newsletterapp.NavigationListener
 import chaymaeidrissi.ma.newsletterapp.R
+import chaymaeidrissi.ma.newsletterapp.adapter.ListArticlesAdapter
+import chaymaeidrissi.ma.newsletterapp.adapter.ListArticlesHandler
+import chaymaeidrissi.ma.newsletterapp.data.FavoriteDataBase
 import chaymaeidrissi.ma.newsletterapp.models.Article
-
-class Favorits {
-
+import chaymaeidrissi.ma.newsletterapp.models.ArticleFavorite
 
 
-    class favorits: Fragment() {
-        lateinit var recyclerView: RecyclerView
-        companion object {
-            fun newInstance(favorits: String): favorits {
-                return favorits().apply {}
-            }
-        }
+class favorits: Fragment() {
+      lateinit var recyclerView: RecyclerView
+       companion object {
+           fun newInstance(favorits: String): favorits {
+              return favorits().apply {}
+         }
+ }
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.favorits, container, false)
-        }
+   override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+           savedInstanceState: Bundle?
+       ): View? {
+           return inflater.inflate(R.layout.favorits, container, false)
+   }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-            recyclerView = view.findViewById(R.id.recycler_view)
-            val articlesfavorits = listOf<Article>()
-            recyclerView.layoutManager = LinearLayoutManager(view.context)
-        }
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+          super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        val articlesfavorits = listOf<Article>()
+         recyclerView.layoutManager = LinearLayoutManager(view.context)
+      }
     }
-}
